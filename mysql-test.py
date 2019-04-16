dc96116439f90e4052fd1bec9fce7a6d15f27dcc
Acedan <joselg05@ucm.es>
2019-04-16 09:33:10 +0200

    Start with some off-camera implementations, such as adapting the code to flask and Rest services or inserting a new user

---
+++ mysql-test.py
@@ -0,0 +1,76 @@
+from flask import Flask, render_template, request
+import pymysql, mysql.connector
+
+app = Flask(__name__)
+
+class Database:
+    def __init__(self):
+        try:
+            connection = mysql.connector.connect(host='127.0.0.1',
+                                         database='tfg',
+                                         user='root',
+                                         password='')
+            self.con = connection
+        except mysql.connector.Error as error:
+            connection.rollback()  # rollback if any exception occured
+            print("Failed connection into python_users table {}".format(error))
+
+    def insert_use(self,userId,users_elo):
+        cursor = self.con.cursor()
+        sql_insert_query = """ INSERT INTO `users_elo` (`user_id`, `users_elo`) VALUES (%s, %s)"""
+        insert_tuple = (userId, users_elo)
+        result = cursor.execute(sql_insert_query,insert_tuple)
+        self.con.commit()
+        print("Record inserted successfully into python_users table")
+
+
+
+@app.route('/users/<user_id>')
+def employees(user_id):
+    data = user_id
+    return render_template('index.html', data=data)
+    #return render_template('index.html', result=res, content_type='application/json')
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html')
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+@app.route('/register')
+def register():
+    return render_template('register.html')
+@app.route('/timeline')
+def timeline():
+    return render_template('timeline.html')
+@app.route('/forms')
+def forms():
+    return render_template('forms.html')
+@app.route('/typography')
+def typography():
+    return render_template('typography.html')
+@app.route('/bootstrap-elements')
+def bootstrapelements():
+    return render_template('bootstrap-elements.html')
+@app.route('/insert_user')
+def insert_user():
+    return render_template('insert-user.html')
+
+@app.route('/new_user',methods=['POST'])
+def usuario():
+
+ userName = request.form['user_id']
+ userELO = request.form['users_elo']
+ if userELO =="":
+     userELO = 8.0
+
+ def db_query():
+     db = Database()
+     db.insert_use(userName,userELO)
+
+ return render_template('insert-user-sucess.html')
+
+
+
