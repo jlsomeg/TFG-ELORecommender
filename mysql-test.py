76578b5e271fc61510c0476a4e6492372491d656
Jose Luis Gomez Alonso <31862196+jlsomeg@users.noreply.github.com>
2019-04-22 01:41:18 +0200

    Update mysql-test.py

--- mysql-test.py
+++ mysql-test.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 class Database:
     def __init__(self):
 
-        self.con = pymysql.connect(host='192.168.0.155',
+        self.con = pymysql.connect(host='localhost',
                                          database='tfg',
                                          user='root',
                                          password='')
